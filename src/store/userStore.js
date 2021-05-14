f3477d23fd153f8a4aa9461c2b77ca32a3cec8da
Marazorq <shuratov2001@mail.ru>
2021-05-14 16:47:13 +0300

    created mobx Store

---
+++ src/store/userStore.js
@@ -0,0 +1,26 @@
+import { makeAutoObservable } from "mobx"
+
+export default class userStore{
+    //вызывается при создании объекта данного класса 
+    constructor(){
+        this._isAuth = false
+        this._user = {}
+        makeAutoObservable(this)// теперь mobx следит за изменениями этих переменных и при изменении будет перерендер
+    }
+
+    //action (функция которая изменяет состояние)
+    setIsAuth(bool){
+        this._isAuth = bool
+    }
+    setUser(bool){
+        this._user = bool
+    }
+    //geter(чтобы получать переменные из нашего состояния), так называемые компьютед функции, они вызываются только в том случае, если переменная внутри была изменена(своего рода оптимизация)
+    get isAuth(){
+        return this._isAuth
+    }
+    get user(){
+        return this._user
+    }
+}
+
